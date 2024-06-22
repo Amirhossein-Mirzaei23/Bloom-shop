f51fbae5eea281cb4a272b7830d58c230367b3f9
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-22 21:47:01 +0330

    create a post webservice to create product in product  model

--- node-project/routes/productsRouter.js
+++ node-project/routes/productsRouter.js
@@ -14,6 +14,38 @@ productsRouter.get('/',(req,res)=>{
         res.json(allProducts)
     })
 })
+// create a product in product model
+productsRouter.post('/create',(req,res)=>{
+    let productInfo = {
+        id:req.body.id,
+        title:req.body.title,
+        type:req.body.type,
+        typeId:req.body.typeId,
+        price:req.body.price,
+        description :req.body.description,
+        image:'@/src/assets/image/products/image1.png',
+        flowerPot:req.body.flowerPot
+    }
 
+    let newProduct = new productsModel(productInfo)
+  
+   newProduct.save().then(()=>{
+     console.log('new product created');
+   })
+   let response = {
+    state:{err:0,msg:'محصول با موفقیت افزوده شد'}
+   }
+   res.json(response)
+})
+
+
+productsRouter.get('/main',(req,res)=>{
+    let userId = req.headers.authorization
+    console.log('productsRouter');
+    productsModel.findById(userId).then((singleProduct)=>{
+        console.log('single product');
+        res.json(singleProduct)
+    })
+})
 
 module.exports = productsRouter
