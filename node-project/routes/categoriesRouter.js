38f78b2d8032ecfeb50ee84ac96ee1f02e04727e
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-23 21:54:03 +0330

    update categories conteroller parameter

--- node-project/routes/categoriesRouter.js
+++ node-project/routes/categoriesRouter.js
@@ -15,5 +15,25 @@ categoriesRouter.get('/',(req,res)=>{
     })
 })
 
+// create a new category in category model
+categoriesRouter.post('/create',(req,res)=>{
+    let categoryInfo = {
+        categoryId:req.body.categoryId,
+        title:req.body.title,
+        description :req.body.description,
+        icon:`@/src/assets/image/categories/+${req.body.icon}`,
+    }
+
+    let newCategory = new categoryModel(categoryInfo)
+  
+    newCategory.save().then(()=>{
+     console.log('new product created');
+   })
+   let response = {
+    state:{err:0,msg:'محصول با موفقیت افزوده شد'}
+   }
+   res.json(response)
+})
+
 
 module.exports = categoriesRouter
