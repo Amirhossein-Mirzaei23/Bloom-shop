0007a56da10e35f19f4d3110bba04aac048e34a9
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-23 21:55:03 +0330

    update users conteroller paramet

--- node-project/routes/userRoute.js
+++ node-project/routes/userRoute.js
@@ -14,5 +14,27 @@ userRoutes.get('/',(req,res)=>{
     })
 })
 
+// create a product in product model
+userRoutes.post('/add',(req,res)=>{
+    let userInfo = {
+        userId:req.body.userId,
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        phoneNumber:req.body.phoneNumber,
+        password:req.body.password,
+        address:req.body.address
+    }
+
+    let newUser = new userModel(userInfo)
+  
+   newUser.save().then(()=>{
+     console.log('new product created');
+   })
+   let response = {
+    state:{err:0,msg:'محصول با موفقیت افزوده شد'}
+   }
+   res.json(response)
+})
+
 
 module.exports = userRoutes
