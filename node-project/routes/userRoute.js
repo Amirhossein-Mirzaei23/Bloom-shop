0a92be7c486c708c59ba77d401f4de32d1bab4a6
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-24 22:39:13 +0330

    create two new webservice to update and delete user from database

--- node-project/routes/userRoute.js
+++ node-project/routes/userRoute.js
@@ -14,6 +14,40 @@ userRoutes.get('/',(req,res)=>{
     })
 })
 
+
+
+
+userRoutes.put('/edit-product/:id',(req,res)=>{
+    console.log('productsRouter');
+    userModel.findOneAndUpdate(
+        {id: req.params.id},
+        {
+        userId:req.params.id,
+        first_name:req.body.first_name,
+        last_name:req.body.last_name,
+        phoneNumber:req.body.phoneNumber,
+        password:req.body.password,
+        address:req.body.address
+    }).then((res)=>{
+        let response = {
+            state:{err:0,msg:'کاربر با موفقیت ویرایش شد'}
+           }
+           res.json(response)
+    })
+
+})
+
+userRoutes.delete('/delte-product/:id',(req,res)=>{
+    console.log('productsRouter');
+    userModel.findOneAndDelete( {id:req.params.id} ).then((user)=>{
+        let response = {
+            state:{err:0,msg:'کاربر با موفقیت حذف شد'}
+           }
+           res.json(response)
+    
+    })
+})
+
 // create a product in product model
 userRoutes.post('/add',(req,res)=>{
     let userInfo = {
