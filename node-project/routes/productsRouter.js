5e7476955b0dcc81be1523357e94148ed95f0539
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-24 22:38:50 +0330

    create two new webservice to update and delete category from database

--- node-project/routes/productsRouter.js
+++ node-project/routes/productsRouter.js
@@ -1,29 +1,68 @@
 const express = require('express')
 
 const productsRouter = express.Router()
+// const multer = require('multer');
 
 const productsModel = require('../models/Products')
 console.log('use');
 // api
 
-productsRouter.get('/',(req,res)=>{
-    
+productsRouter.get('/all-products',(req,res)=>{
     console.log('productsRouter');
     productsModel.find({}).then((allProducts)=>{
         console.log('allproduct');
         res.json(allProducts)
     })
 })
+
+productsRouter.put('/edit-product/:id',(req,res)=>{
+    console.log('productsRouter');
+     productsModel.findOneAndUpdate(
+        {id: req.params.id},
+        {
+        id:req.params.id,
+        title:req.body.title,
+        category:req.body.type,
+        categoryId:req.body.typeId,
+        salePrice:req.body.salePrice,
+        offPrice:req.body.offPrice,
+        dollerPrice:req.body.dollerPrice,
+        immediateDelivery:req.body.immediateDelivery,
+        image: req.body.icon?  `@/src/assets/image/products/+${req.body.icon}`:`@/src/assets/image/noImage.png` ,
+        flowerPot:req.body.flowerPot
+    }).then((res)=>{
+        let response = {
+            state:{err:0,msg:'محصول با موفقیت ویرایش شد'}
+           }
+           res.json(response)
+    })
+
+})
+
+productsRouter.delete('/delte-product/:id',(req,res)=>{
+    console.log('productsRouter');
+    productsModel.findOneAndDelete( {id:req.params.id} ).then((allProducts)=>{
+        let response = {
+            state:{err:0,msg:'محصول با موفقیت حذف شد'}
+           }
+           res.json(response)
+    
+    })
+})
+
+
 // create a product in product model
 productsRouter.post('/create',(req,res)=>{
     let productInfo = {
         id:req.body.id,
         title:req.body.title,
-        type:req.body.type,
-        typeId:req.body.typeId,
-        price:req.body.price,
-        description :req.body.description,
-        image:`@/src/assets/image/products/+${req.body.icon}`,
+        category:req.body.type,
+        categoryId:req.body.typeId,
+        salePrice:req.body.salePrice,
+        offPrice:req.body.offPrice,
+        dollerPrice:req.body.dollerPrice,
+        immediateDelivery:req.body.immediateDelivery,
+        image: req.body.icon?  `@/src/assets/image/products/+${req.body.icon}`:`@/src/assets/image/noImage.png` ,
         flowerPot:req.body.flowerPot
     }
 
