b027ab7e802ec0c15cabed56d315e87924a5dc8b
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-26 23:18:30 +0330

    update nuxt app

--- nuxt-app/components/Card.vue
+++ nuxt-app/components/Card.vue
@@ -2,18 +2,20 @@
     <div class="container  mt-5 mx-auto " >
         <div class="mx-auto mb-5" ><h1 class="mx-auto text-center font-bold text-2xl" >ارسالی ها امروز </h1></div>
         <hr/>
-   
    <div class="flex gap-5 items-center justify-center justify-items-center mt-5 " >
-        <div   v-for="product in products"  :key="product" class="group  hover:scale-110 transition-all duration-500 w-3/12 p-2 bg-gray-200 rounded-xl  flex flex-col items-center justify-items-center" >
-<div class="rounded-lg overflow-hidden flex items-center justify-items-center max-h-72" >
-    <img class="" src='~/assets/image/cardImage.jpeg' :alt="product.orginalName">
+        <div    v-for="product in  productsData"  :key="product" class="group  hover:scale-110 transition-all duration-500 w-3/12 p-2 bg-gray-200 rounded-xl  flex flex-col items-center justify-items-center" >
+
+
+            
+            <div class="rounded-lg overflow-hidden flex items-center justify-items-center max-h-72" >
+    <img class="" src='~/assets/image/cardImage.jpeg' >
 </div>
 <div class="flex flex-col items-center" >
-    <h3>{{ product.name }}</h3>
-    <h6>{{ product.orginalName }}</h6>
+    <h3>{{ product.title }}</h3>
+   
 </div>
 <div class="grid grid-cols-3 " >
-<div class="text-rose-500  col-span-2 text-xl font-bold" >{{ product.price }}<span  class="text-gray-500 ml-5 text-sm font-extralight mr-1" >تومان</span></div>
+<div class="text-rose-500  col-span-2 text-xl font-bold" >{{ product.salePrice }}<span  class="text-gray-500 ml-5 text-sm font-extralight mr-1" >تومان</span></div>
 <div class="text-rose-500 border-r-2 border-red-600 text-xl font-bold " >{{ product.dollerPrice }}<span class="text-gray-500 ml-1 text-sm font-extralight mr-5" >$</span></div>
 </div>
 
@@ -25,74 +27,41 @@
 </template>
 
 <script setup>
+
 let isAccess = ref(false)
+const productsData = ref()
 const {public:{apiBase}} = useRuntimeConfig();
 
-async function getData(){
+import axios from "axios";
+
+///
+
+  function getData(){
 console.log("get data");
 try {
-
-    const {data,err} = await useFetch(`${apiBase}/users`,{
-        method:'GET',
-        headers:{'content-type':'application/json'},
-        Origin: 'http://localhost:3000',
-        
-    })
-
-    const products = await useFetch(`${apiBase}/products`,{
-        method:'GET'  
+    axios.post(`${apiBase}/products`,{
+   
+        categoryId:5
+   
     }).then((res)=>{
-        console.log(res.data);
+        
+        productsData.value = res.data
+        console.log(productsData.value);
+        toastr.success("  باموفقیت انجام شد");
     })
 
-console.log(data);
-console.log(err);
-
+  
 } catch (error) {
 console.log('err');
     console.log(error);
-
+    toastr.error(" با خطا مواجه انجام شد");
 }
 }
 
-async function createdata(){
 
-console.log("create");
-
-try {
-    const {create,error} = await useFetch(`${apiBase}/createUser`,{
-    query:{
-        id:1,
-        name:'گل نازنین',
-        price:1000,
-        englishName:"naziFlower",
-        dollerPrice:5
-    }
+onBeforeMount(()=>{
+    getData()
 })
-
-console.log(create);
-console.log(error);
-} catch (error) {
-    console.log(error);
-}
-
-}
-
-let products = [
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-    {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-     {id:4,name:"گل امیر",price:15000,orginalName:'Yas Flower Envelope',dollerPrice:5,image:'~/assets/image/cardImage.jpeg'},
-]
-
-
 
 </script>
 
