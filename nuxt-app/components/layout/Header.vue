467ffff7359ad8d16a2ea4db819973cd68cc30f0
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-27 23:35:13 +0330

    creat a new style for header navbar and create a icon for slider menu

--- nuxt-app/components/layout/Header.vue
+++ nuxt-app/components/layout/Header.vue
@@ -1,100 +1,59 @@
 <template>
     <div class="text-black">
-    <div class="container  grid grid-cols-3 h-16 mx-auto text-sm text-nowrap font-extralight  " >
-        <div class="col-span-2  flex flex-row items-center " >
-         <!----  <ul class=" flex flex-row items-center gap-5 justify-items-center " >
-                <li>صفحه اصلی</li>
-                <li>دسته گل</li>
-                <li>گیاه اپارتمانی</li>
-                <li>ارسال هدیه به کل ایران</li>
-                <li> رز جاویدان</li>
-                <li>ارسال امروز</li>
-                ---> 
-             
-                    <div class="q-pa-md">
-                      <q-btn-group class="rounded-lg hover:shadow-xl hover:shadow-teal-400 transition-all duration-500" >
-                        <q-btn  class="text-black"   color="bg-gray-400" label="صفحه اصلی" />
-                        <q-btn-dropdown auto-close class="text-black "  color="primary" label="قروشگاه" split>
+    <div class="flex justify-between flex-nowrap flex-row h-16  mx-auto text-sm text-nowrap font-extralight " >
+     
+        <div class="col-span-2  max-md:hidden" > 
+                    <div class=" w-full text-xs   mx-auto ">
+                      <q-btn-group class="xl:mr-5 lg:mr-1 mr-0 rounded-lg  hover:shadow-xl  hover:shadow-teal-400 transition-all duration-500 mt-4" >
+                        <q-btn  class="text-black text-xs lg:text-sm w-16 lg:w-20 max-lg:hidden"  to="/"  color="bg-gray-400" label="صفحه اصلی" />
+                        <q-btn-dropdown auto-close class="text-black text-xs lg:text-sm  md:w-28 lg:w-auto "  color="primary" label="فروشگاه" split>
                             <!-- dropdown content goes here -->
-                            <q-list class="bg-gradient-to-r from-gray-400 via-white to-gray-200 mt-3" padding style="width: 250px">
-                              <q-item clickable>
+                            <q-list class="bg-gradient-to-r from-gray-400 via-white to-gray-200  " padding style="width: 250px">
+                             
+                              <q-item clickable  v-for="category in categoriesTilte" :key="category"  :to="`/productList/${category.categoryId}`" >
                                 <q-item-section avatar>
                                   <q-avatar color="green" text-color="white" >
                                     <img src="../../assets/icons/bi/flower3.svg" alt="">
                                   </q-avatar>
                                 </q-item-section>
                                 <q-item-section>
-                                  <q-item-label>دسته گل</q-item-label>
-                                  <q-item-label caption>Lorem, ipsum dolor.
+                                  <q-item-label>{{category.title}}</q-item-label>
+                                  <q-item-label caption><span class="text-xs" >{{ category.description }}</span>
                                   </q-item-label>
                                 </q-item-section>
-                                <q-item-section side>
-                                  <q-icon name="info" color="amber" />
-                                </q-item-section>
+                              <!-----  <q-item-section side>
+                                  <q-icon name="info" color="info" /> 
+                                 
+                                </q-item-section> ---->
                               </q-item>
                     
-                              <q-item clickable>
-                                <q-item-section avatar>
-                                  <q-avatar color="green" text-color="white" >
-                                    <img src="../../assets/icons/bi/tree.svg" alt="">
-                                  </q-avatar>
-
-                                </q-item-section>
-                                <q-item-section>
-                                  <q-item-label>گیاهان اپرتمانی</q-item-label>
-                                  <q-item-label caption>Lorem, ipsum dolor.</q-item-label>
-                                </q-item-section>
-                                <q-item-section side>
-                                  <q-icon name="info" color="amber" />
-                                </q-item-section>
-                              </q-item>
+                              
                     
                               <q-separator inset />
-                              <q-item-label header>Lorem, ipsum dolor.</q-item-label>
+                              <q-item-label header>by happiness</q-item-label>
                     
-                              <q-item clickable>
-                                <q-item-section avatar>
-                                  <q-avatar icon="assignment" color="teal" text-color="white" />
-                                </q-item-section>
-                                <q-item-section>
-                                  <q-item-label>London</q-item-label>
-                                  <q-item-label caption>March 1st, 2018</q-item-label>
-                                </q-item-section>
-                                <q-item-section side>
-                                  <q-icon name="info" color="amber" />
-                                </q-item-section>
-                              </q-item>
+                         
                     
-                              <q-item clickable>
-                                <q-item-section avatar>
-                                  <q-avatar icon="assignment" color="teal" text-color="white" />
-                                </q-item-section>
-                                <q-item-section>
-                                  <q-item-label>Paris</q-item-label>
-                                  <q-item-label caption>January 22nd, 2017</q-item-label>
-                                </q-item-section>
-                                <q-item-section side>
-                                  <q-icon name="info" color="amber" />
-                                </q-item-section>
-                              </q-item>
+                            
                             </q-list>
                           </q-btn-dropdown>
-     <!-----                   <q-btn  class="text-black"  color="bg-gray-400" label="دسته گل" />
-                        <q-btn  class="text-black"  color="bg-gray-400" label="گیاهان اپارتمانی" />
-                        <q-btn  class="text-black"  color="bg-gray-400" label="رز جاویدان" />
-                        <q-btn  class="text-black"  color="bg-gray-400" label="ارسالی های امروز" />
-                   ------->
-                   <q-btn  v-for="category in categoriesTilte" :key="category"  :to="`/productList/${category.categoryId}`"  class="text-black"  color="bg-gray-400" :label="category.title" />
-                   <q-btn  class="text-black" to="/admin/create"  color="bg-gray-400" label="ایجاد محصول" />
+   
+                   <q-btn  v-for="category in categoriesTilte" :key="category"  :to="`/productList/${category.categoryId}`"  class="text-black  text-xs lg:text-sm md:w-20 lg:w-auto "  color="bg-gray-400" :label="category.title" />
+                   <q-btn  class="text-black  text-xs lg:text-sm " to="/admin/create"  color="bg-gray-400" label="ایجاد محصول" />
                   </q-btn-group>
                     </div>
-                 
-            
         </div>
-        <div class=" flex " >
-           
+        <div>
+          <button class="md:hidden " ><img class="w-16" src="../../assets/menu.gif" alt=""></button>
+        </div>
+        <div class="flex flex-row xl:ml-5 lg:ml-3 ml-1" >
+           <div>
+           <!----- <q-avatar>
+              <img src="../../assets/icons/social/soc003.svg" alt="">
+            </q-avatar> --->
+           </div>
                <div class="bg-red-300 m-auto rounded-lg overflow-hidden" >
-                <img class="m-auto" src="../../assets/image/logo.png" alt="">
+                <img class="m-auto max-w-32" src="../../assets/image/logo.png" alt="">
                </div>
           
  
