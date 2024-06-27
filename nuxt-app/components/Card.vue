11362cf7ca14651c3c384be8a1baa75f77e9bc6a
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-27 23:29:33 +0330

    mange reposnsive design

--- nuxt-app/components/Card.vue
+++ nuxt-app/components/Card.vue
@@ -3,25 +3,7 @@
         <div class="mx-auto mb-5" ><h1 class="mx-auto text-center font-bold text-2xl" >ارسالی ها امروز </h1></div>
         <hr/>
    <div class="flex gap-5 items-center justify-center justify-items-center mt-5 " >
-        <div    v-for="product in  productsData"  :key="product" class="group  hover:scale-110 transition-all duration-500 w-3/12 p-2 bg-gray-200 rounded-xl  flex flex-col items-center justify-items-center" >
-
-
-            
-            <div class="rounded-lg overflow-hidden flex items-center justify-items-center max-h-72" >
-    <img class="" src='~/assets/image/cardImage.jpeg' >
-</div>
-<div class="flex flex-col items-center" >
-    <h3>{{ product.title }}</h3>
-   
-</div>
-<div class="grid grid-cols-3 " >
-<div class="text-rose-500  col-span-2 text-xl font-bold" >{{ product.salePrice }}<span  class="text-gray-500 ml-5 text-sm font-extralight mr-1" >تومان</span></div>
-<div class="text-rose-500 border-r-2 border-red-600 text-xl font-bold " >{{ product.dollerPrice }}<span class="text-gray-500 ml-1 text-sm font-extralight mr-5" >$</span></div>
-</div>
-
-
-<div class="mx-auto mt-5" ><button @dblclick="createdata"  @click="getData" class="font-extralight text-sm opacity-0 group-hover:opacity-100 transition-all duration-500 animate-bounce bg-rose-700 rounded-2xl  mx-auto text-nowrap px-2 py-1" >سفارش این محصول</button></div>       
-    </div>
+    <ProductCard  v-for="product in  productsData"  :key="product" :productsData="product" ></ProductCard>
 </div>
 </div>
 </template>
@@ -46,8 +28,7 @@ try {
     }).then((res)=>{
         
         productsData.value = res.data
-        console.log(productsData.value);
-        toastr.success("  باموفقیت انجام شد");
+      
     })
 
   
