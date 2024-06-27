2370b61887e35ea9d1dfdb73af2391f5d1002436
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-27 23:29:53 +0330

    mange reposnsive design

--- nuxt-app/components/productCard.vue
+++ nuxt-app/components/productCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div   class="group  hover:scale-110 transition-all duration-500 w-3/12 p-2 bg-gray-200 rounded-xl  flex flex-col items-center justify-items-center" >
+    <div   class="group  hover:scale-110 transition-all duration-500 w-5/12 md:w-3/12 p-2 bg-gray-200 rounded-xl  flex flex-col items-center justify-items-center" >
             
         <div class="rounded-lg overflow-hidden flex items-center justify-items-center max-h-72" >
 <img class="" src='~/assets/image/cardImage.jpeg' >
@@ -8,9 +8,11 @@
 <h3>{{ product.productsData.title }}</h3>
 
 </div>
-<div class="grid grid-cols-3 " >
-<div class="text-rose-500  col-span-2 text-xl font-bold" >{{ product.productsData.salePrice }}<span  class="text-gray-500 ml-5 text-sm font-extralight mr-1" >تومان</span></div>
-<div class="text-rose-500 border-r-2 border-red-600 text-xl font-bold " >{{ product.productsData.dollerPrice }}<span class="text-gray-500 ml-1 text-sm font-extralight mr-5" >$</span></div>
+<div class="sm:grid grid-cols-3 text-xs  mx-auto" >
+<div class="text-rose-500  text-xs col-span-2 xl:text-xl font-bold  md:text-sm lg:text-lg  text-center" >{{ product.productsData.salePrice }}
+    <span  class="text-gray-500 ml-5 text-sm font-extralight mr-1" >تومان</span>
+</div>
+<div class="text-rose-500 text-xs border-t-2 rounded-lg sm:border-r-2 border-red-600 md:text-sm lg:text-lg xl:text-xl font-bold  text-end " >{{ product.productsData.dollerPrice }}<span class="text-gray-500 ml-1 text-sm font-extralight mr-5" >$</span></div>
 </div>
 
 
