4b6fca90f24be0ae6a5a53744c2344cd842f8bf9
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-06-27 23:30:01 +0330

    mange reposnsive design

--- nuxt-app/components/tabPanel.vue
+++ nuxt-app/components/tabPanel.vue
@@ -1,11 +1,11 @@
 <template>
     <div class=" container grid grid-cols-6 m-auto bg-white p-5 rounded-xl " >
-        <div class="flex flex-col items-center" ><img src="../assets/image/دسته-گل.jpg" alt=""><p>دسته گل</p></div>
-        <div class="flex flex-col items-center" ><img src="../assets/image/گیاه-اپارتمانی.jpg" alt=""><p>گیاه اپارتمانی</p></div>
-        <div class="flex flex-col items-center" ><img src="../assets/image/گل-تولد.png" alt=""><p>گل تولد</p></div>
-        <div class="flex flex-col items-center" ><img src="../assets/image/دسته-گل.jpg" alt=""><p>دسته گل</p></div>
-        <div class="flex flex-col items-center" ><img src="../assets/image/ارسال-گل-به-ایران.png" alt=""><p>ارسال-گل به ایران</p></div>
-        <div class="flex flex-col items-center" ><img src="../assets/image/رز-جاودان.png" alt=""><p>رز جاودان</p></div>
+        <div class="flex flex-col items-center text-xs md:text-sm text-center" ><img src="../assets/image/دسته-گل.jpg" alt=""><p>دسته گل</p></div>
+        <div class="flex flex-col items-center text-xs md:text-sm text-center" ><img src="../assets/image/گیاه-اپارتمانی.jpg" alt=""><p>گیاه اپارتمانی</p></div>
+        <div class="flex flex-col items-center text-xs md:text-sm text-center" ><img src="../assets/image/گل-تولد.png" alt=""><p>گل تولد</p></div>
+        <div class="flex flex-col items-center text-xs md:text-sm text-center" ><img src="../assets/image/دسته-گل.jpg" alt=""><p>دسته گل</p></div>
+        <div class="flex flex-col items-center text-xs md:text-sm text-center" ><img src="../assets/image/ارسال-گل-به-ایران.png" alt=""><p>ارسال گل در ایران</p></div>
+        <div class="flex flex-col items-center text-xs md:text-sm text-center" ><img src="../assets/image/رز-جاودان.png" alt=""><p>رز جاودان</p></div>
     </div>
 </template>
 
