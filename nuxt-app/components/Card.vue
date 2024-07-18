3974efe824b3e98ffab4d457ce41f44fe67f1ad3
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-07-18 23:06:09 +0330

    update style of product  card

--- nuxt-app/components/Card.vue
+++ nuxt-app/components/Card.vue
@@ -6,7 +6,7 @@
 <div class="w-11/12 mx-auto" >
     <div class="mx-auto mb-5 max-w-80 " ><h1 class="mx-auto text-center font-bold text-2xl" >ارسالی ها امروز </h1></div>
         <hr/> 
-   <div class=" flex gap-5 items-center justify-center  mt-5 " >
+   <div class=" grid grid-cols-2 md:grid-cols-3 gap-5 items-center justify-center  mx-auto mt-5 " >
     <ProductCard  v-for="product in  productsData"  :key="product" :productsData="product" ></ProductCard>
 </div>
 </div>
