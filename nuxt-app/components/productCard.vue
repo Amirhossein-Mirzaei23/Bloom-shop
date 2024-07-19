cdee4540cb31bbfe96c4a1582d1818b892a85eb1
Amirhossein-Mirzaei23 <139608937+Amirhossein-Mirzaei23@users.noreply.github.com>
2024-07-19 22:50:58 +0330

    asign add product to cart webservice in product card

--- nuxt-app/components/productCard.vue
+++ nuxt-app/components/productCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div   class="flex flex-wrap max-w-44 md:max-w-52 lg:max-w-60 group transition-all duration-500 p-2 bg-gray-200 rounded-xl   items-center justify-items-center" >
+    <div   class="flex flex-wrap max-w-44 md:max-w-52 lg:max-w-60 group transition-all duration-500 p-2 bg-gray-200 bg-opacity-70 rounded-xl   items-center justify-items-center" >
             
         <div class="w-40 md:w-48 lg:w-72 rounded-lg overflow-hidden flex items-center justify-items-center max-h-72" >
 <img class="w-36 md:w-44 lg:w-52 scale-125 m-auto" src='~/assets/image/cardImage.jpeg' >
@@ -7,6 +7,7 @@
 
 <div class="flex flex-col items-center" >
     <h3>{{ product.productsData.title }}</h3>
+    <h6>{{product.productsData.id}}</h6>
 </div>
 
 <div class="flex flex-nowrap text-xs  mx-auto gap-1 px-2" >
@@ -21,13 +22,36 @@
 </div>
 
 
-<div class="mx-auto mt-5" ><button  @click="getData" class="font-extralight text-sm opacity-0 group-hover:opacity-100  animate__animated animate__pulse animate__infinite	 bg-rose-700 rounded-lg  mx-auto text-nowrap px-2 py-1 hover:tracking-tight duration-300 transition-all"  >سفارش این محصول</button></div>       
+<div class="mx-auto mt-5" ><button  @click="addToCart" class="font-extralight text-sm opacity-0 group-hover:opacity-100  animate__animated animate__pulse animate__infinite	 bg-rose-700 rounded-lg  mx-auto text-nowrap px-2 py-1 hover:tracking-tight duration-300 transition-all"  >سفارش این محصول</button></div>       
 </div>
 </template>
 
 <script setup>
 
+import axios from 'axios'
+
+const {public:{apiBase}} = useRuntimeConfig();
+
+
 const product = defineProps(['productsData'])
+const user = JSON.parse(localStorage.getItem('user'))
+function addToCart(){
+    axios.post(`${apiBase}/userCart/addToCart`,{
+        userId:user.userId,
+
+        
+        products:[{
+            productId:product.productsData.id,
+            name:product.productsData.title,
+            price:product.productsData.salePrice,
+            quantity:1
+        }]
+    }).then((res)=>{
+toastr.success(res.data.state.msg)
+    }).catch((err)=>{
+        toastr.error(err.response.state.msg)
+    })
+}
 
 </script>
 
